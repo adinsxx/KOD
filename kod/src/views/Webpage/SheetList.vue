1a4741e1637c7cf2396d15ec609abf1ea5284029
Connor <c.mccurrie@gmail.com>
2021-12-12 20:02:50 -0600

    1. Sheet cards were overlapping with Navbar; Resolved

--- kod/src/views/Webpage/SheetList.vue
+++ kod/src/views/Webpage/SheetList.vue
@@ -1,17 +1,75 @@
 <template>
   <div>
-    <h2>My Sheets</h2>
+
+
+    <v-app-bar
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>My Sheets</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+    >
+      <v-list
+          nav
+          dense
+      >
+        <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              <router-link to="/" class="text-decoration-none">
+                Home
+              </router-link>
+            </v-list-item-title>
+
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              <router-link to="/profile" class="text-decoration-none">
+                Profile
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-file</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              <router-link to="/sheet-pages" class="text-decoration-none">
+                My Sheets
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-row class="sheet-list" v-if="characters">
-      <v-card cols="12" sm="3" md="4" class="mx-0" v-for="character in characters" :key="character.id" :character="character">
-        <ul>Name: {{character.name}}</ul>
-        <ul>Race: {{character.raceName}} | Class: {{character.className}}</ul>
-        <ul>Background: {{character.background}}</ul>
-        <ul>Ability Scores: {{character.abilityScores}}</ul>
-        <ul>Proficiencies: {{character.proficiences}}</ul>
-        <ul>Options: {{character.options}}</ul>
-        <ul>Spells: {{character.spells}} </ul>
-        <ul>Equipment: {{character.equipment}}</ul>
-        <ul>Description: {{character.description}}</ul>
+      <v-card class="pa-md-4 mx-lg-auto" width="500px" v-for="character in characters" :key="character.id" :character="character">
+        <p>Name: {{character.name}}</p>
+        <p>Race: {{character.raceName}}</p>
+        <p>Class: {{character.className}}</p>
+        <p>Background: {{character.background}}</p>
+        <p>Ability Scores: {{character.abilityScores}}</p>
+        <p>Proficiencies: {{character.proficiences}}</p>
+        <p>Options: {{character.options}}</p>
+        <p>Spells: {{character.spells}} </p>
+        <p>Equipment: {{character.equipment}}</p>
+        <p>Description: {{character.description}}</p>
+
       </v-card>
     </v-row>
   </div>
@@ -35,6 +93,8 @@ export default {
   data(){
     return {
       characters: [],
+      drawer: false,
+      group: null,
     }
   },
 
@@ -48,5 +108,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .sheet-list{
+    margin-top: 100px;
+  }
 </style>
